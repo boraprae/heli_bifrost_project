64321412b6cbc349affd6565d21bc873a9db3b12
boraprae <72434691+boraprae@users.noreply.github.com>
2022-04-01 00:00:28 +0700

    chat layout and demo data (bug: right overflow)

--- lib/chats/chatScreen.dart
+++ lib/chats/chatScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:heli_bifrost_project/components/message.dart';
+import 'package:heli_bifrost_project/chats/components/chat_input_field.dart';
+import 'package:heli_bifrost_project/chats/components/message.dart';
 import 'package:heli_bifrost_project/constants.dart';
 import 'package:heli_bifrost_project/models/chatMessage.dart';
-
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,6 +12,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  var selectedItem = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +22,18 @@ class _ChatScreenState extends State<ChatScreen> {
           children: [
             BackButton(),
             CircleAvatar(
-              backgroundImage: AssetImage("assets/images/user_2.png"),
+              backgroundImage: AssetImage("assets/images/heli_profile.jpg"),
             ),
             SizedBox(width: kDefaultPadding * 0.75),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Kristin Watson",
-                  style: TextStyle(fontSize: 16),
+                  "สถานะวันนี้ของเฮลิ",
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
                 Text(
-                  "Active 3m ago",
+                  "โชคดีที่มีเฮลิ",
                   style: TextStyle(fontSize: 12),
                 )
               ],
@@ -40,14 +41,29 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
         actions: [
-          IconButton(
-            icon: Icon(Icons.local_phone),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.videocam),
-            onPressed: () {},
-          ),
+          //!--- Assume the menu ---!
+          PopupMenuButton(onSelected: (value) {
+            setState(() {
+              selectedItem = value.toString();
+            });
+            //print(value);
+            Navigator.pushNamed(context, value.toString());
+          }, itemBuilder: (BuildContext bc) {
+            return const [
+              PopupMenuItem(
+                child: Text("Hello"),
+                value: '/hello',
+              ),
+              PopupMenuItem(
+                child: Text("About"),
+                value: '/about',
+              ),
+              PopupMenuItem(
+                child: Text("Contact"),
+                value: '/contact',
+              )
+            ];
+          }),
           SizedBox(width: kDefaultPadding / 2),
         ],
       ),
@@ -56,86 +72,14 @@ class _ChatScreenState extends State<ChatScreen> {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: ListView.builder(
-                itemCount: demeChatMessages.length,
-                itemBuilder: (context, index) =>
-                    Message(message: demeChatMessages[index]),
-              ),
+              // child: ListView.builder(
+              //   itemCount: demeChatMessages.length,
+              //   itemBuilder: (context, index) =>
+              //       Message(message: demeChatMessages[index]),
+              // ),
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: kDefaultPadding,
-              vertical: kDefaultPadding / 2,
-            ),
-            decoration: BoxDecoration(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 4),
-                  blurRadius: 32,
-                  color: Color(0xFF087949).withOpacity(0.08),
-                ),
-              ],
-            ),
-            child: SafeArea(
-              child: Row(
-                children: [
-                  Icon(Icons.mic, color: Colors.purple),
-                  SizedBox(width: kDefaultPadding),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: kDefaultPadding * 0.75,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.purple.withOpacity(0.05),
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.sentiment_satisfied_alt_outlined,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.64),
-                          ),
-                          SizedBox(width: kDefaultPadding / 4),
-                          Expanded(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: "Type message",
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                          Icon(
-                            Icons.attach_file,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.64),
-                          ),
-                          SizedBox(width: kDefaultPadding / 4),
-                          Icon(
-                            Icons.camera_alt_outlined,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.64),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          ChatInputField(),
         ],
       ),
     );
